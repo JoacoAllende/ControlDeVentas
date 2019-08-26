c5c0b252d83111d10d82ff45042afe39c71c2d01
Joaquin Allende <joacoallendeids@gmail.com>
2019-08-26 19:48:44 -0300

    Arregla package.json

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 require('./database');
 
@@ -13,11 +13,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:4200'}))
+// app.use(cors({origin: 'http://localhost:4200'}))
 
 // Routes
 
-app.use(require('./routes/series.routes'));
+app.use(require('./routes/producto.routes'));
 
 // Starting the server
 
