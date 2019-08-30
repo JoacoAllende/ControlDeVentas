c38f4ef5db0a5cab1c5bcebf1885fb2209177209
Joaquin Allende <joacoallendeids@gmail.com>
2019-08-30 20:01:30 -0300

    arregla base inicial

--- Database/2019-08-26-Initial-database.sql
+++ Database/2019-08-26-Initial-database.sql
@@ -5,6 +5,7 @@ CREATE TABLE cliente (
 
 CREATE TABLE producto (
     id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    codigo INT(15);
     descripcion VARCHAR(80) NOT NULL,
     precio NUMERIC NOT NULL
 );
