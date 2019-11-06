008c72da7211afcc19238480cac708364dcd1819
Joaquin Allende <joacoallendeids@gmail.com>
2019-11-06 16:57:12 -0300

    Modifica base de datos para facturacion

--- Database/2019-08-26-Initial-database.sql
+++ Database/2019-08-26-Initial-database.sql
@@ -1,6 +1,9 @@
 CREATE TABLE cliente (
     id INT(4) AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
+    nombre VARCHAR(50) NOT NULL,
+    doc_tipo INT(2) NOT NULL,
+    doc_nro BIGINT(14) NOT NULL,
+    telefono VARCHAR(18) NULL
 );
 
 CREATE TABLE producto (
@@ -15,6 +18,7 @@ CREATE TABLE venta (
     id_cliente INT(4) NOT NULL,
     fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
     total NUMERIC(5,2) NOT NULL,
+    facturado BOOLEAN DEFAULT false,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
 
@@ -27,4 +31,16 @@ CREATE TABLE detalle_venta (
     PRIMARY KEY (id_venta, id_detalle),
     FOREIGN KEY (id_venta) REFERENCES venta(id),
     FOREIGN KEY (id_producto) REFERENCES producto(id)
+);
+
+CREATE TABLE factura (
+    id_venta INT(6) PRIMARY KEY,
+    nro_cae BIGINT(16) UNIQUE,
+    fecha_emision DATE NOT NULL,
+    cbte_tipo INT(3) NOT NULL,
+    pto_venta INT(2) NOT NULL,
+    nro_comprobante INT(6) UNIQUE,
+    id_cliente INT(4) NOT NULL,
+    FOREIGN KEY (id_venta) REFERENCES venta(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
