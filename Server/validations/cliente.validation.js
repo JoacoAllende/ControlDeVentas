05f7ef8d58593e521bfe9c07911c067b24d1bad9
Joaquin Allende <joacoallendeids@gmail.com>
2019-11-13 21:27:10 -0300

    Agrega verificaciones en front y back para el ABM de clientes

--- Server/validations/cliente.validation.js
+++ Server/validations/cliente.validation.js
@@ -3,7 +3,23 @@ const clienteValidator = {};
 const clienteController = require('../controllers/cliente.controller');
 
 clienteValidator.validar_createCliente = (req, res) => {
-    clienteController.createCliente(req, res);
+    const cliente = req.body;
+    const doc_tipo = cliente.doc_tipo;
+    const doc_nro = cliente.doc_nro;
+    let clienteValido = false;
+    if (doc_tipo == 80 || doc_tipo == 86) {
+        if (CuitCuilValido(doc_nro)) {
+            clienteController.createCliente(req, res);
+            clienteValido = true;
+        }
+    } else if (doc_tipo == 96) {
+        if (dniValido(doc_nro)) {
+            clienteController.createCliente(req, res);
+            clienteValido = true;
+        }
+    }
+    if (!clienteValido)
+        res.json('Invalid client');
 }
 
 clienteValidator.validar_getClientes = (req, res) => {
@@ -15,11 +31,65 @@ clienteValidator.validar_getAllClientes = (req, res) => {
 }
 
 clienteValidator.validar_deleteCliente = (req, res) => {
-    clienteController.deleteCliente(req, res);
+    const id = req.params.id;
+    if (!isNaN(id)) {
+        clienteController.deleteCliente(req, res);
+    }
+    else
+        res.json('error');
 }
 
 clienteValidator.validar_updateCliente = (req, res) => {
-    clienteController.updateCliente(req, res);
+    const cliente = req.body;
+    let clienteValido = false;
+    const id = cliente.id;
+    if (!isNaN(id)) {
+        const doc_tipo = cliente.doc_tipo;
+        const doc_nro = cliente.doc_nro;
+        if (doc_tipo == 80 || doc_tipo == 86) {
+            if (CuitCuilValido(doc_nro)) {
+                clienteController.updateCliente(req, res);
+                clienteValido = true;
+            }
+        } else if (doc_tipo == 96) {
+            if (dniValido(doc_nro)) {
+                clienteController.updateCliente(req, res);
+                clienteValido = true;
+            }
+        }
+    }
+    if (!clienteValido)
+        res.json('Invalid client');
+}
+
+CuitCuilValido = (cuit) => {
+    if (cuit.toString().length != 11) {
+        return false;
+    }
+    var acumulado = 0;
+    var digitos = cuit.toString().split("");
+    var digito = digitos.pop();
+    for (var i = 0; i < digitos.length; i++) {
+        acumulado += digitos[9 - i] * (2 + (i % 6));
+    }
+    var verif = 11 - (acumulado % 11);
+    if (verif == 11) {
+        verif = 0;
+    }
+    if (digito != verif) {
+        return false
+    } else {
+        return true;
+    }
+}
+
+dniValido = (dni) => {
+    if (dni.toString().length != 8) {
+        alert("El DNI debe ser de 8 caracteres");
+        return false;
+    } else {
+        return true;
+    }
 }
 
 module.exports = clienteValidator;
--- frontend/src/app/components/cliente/cliente.component.ts
+++ frontend/src/app/components/cliente/cliente.component.ts
@@ -40,7 +40,10 @@ export class ClienteComponent implements OnInit {
     this.inicial = false;
   }
 
-  habilitarInicial() {
+  habilitarInicial(form?: NgForm) {
+    if (form) {
+      this.resetForm(form);
+    }
     this.edicion = false;
     this.inicial = true;
     this.clientesObs.subscribe(cl => this.clientes = cl);
@@ -54,9 +57,12 @@ export class ClienteComponent implements OnInit {
 
   CheckValidation(form: NgForm) {
     var formulario = document.forms["clienteForm"];
+    if (form.value.telefono == null)
+      form.value.telefono = '';
     for (let i = 0; i < formulario.length; i++) {
-      if (formulario[i].type != 'hidden' && (formulario[i].tagName == 'INPUT' || formulario[i].tagName == 'SELECT'))
+      if (formulario[i].type != 'hidden' && (formulario[i].tagName == 'INPUT' || formulario[i].tagName == 'SELECT') && formulario[i].name != 'telefono')
         if (formulario[i].value == '') {
+          console.log(formulario[i]);
           alert('Debe completarse el campo ' + formulario[i].name);
           return;
         } else if (formulario[i].name === 'precio' && formulario[i].value < 0) {
@@ -67,41 +73,86 @@ export class ClienteComponent implements OnInit {
     this.addCliente(form);
   }
 
-  addCliente(form : NgForm){
-    if (form.value.id == null){
+  CuitCuilValido(cuit) {
+    if (cuit.toString().length != 11) {
+      alert("El CUIT/CUIL debe ser de 11 caracteres");
+      return false;
+    }
+    var acumulado = 0;
+    var digitos = cuit.toString().split("");
+    var digito = digitos.pop();
+    for (var i = 0; i < digitos.length; i++) {
+      acumulado += digitos[9 - i] * (2 + (i % 6));
+    }
+    var verif = 11 - (acumulado % 11);
+    if (verif == 11) {
+      verif = 0;
+    }
+    if (digito != verif) {
+      alert("El CUIT/CUIL ingresado es inválido");
+      return false
+    } else {
+      return true;
+    }
+  }
+
+  dniValido(dni) {
+    if (dni.toString().length != 8) {
+      alert("El DNI debe ser de 8 caracteres");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  addCliente(form: NgForm) {
+    if (form.value.doc_tipo == 80 || form.value.doc_tipo == 86) {
+      if (this.CuitCuilValido(form.value.doc_nro)) {
+        this.agregarCliente(form);
+      }
+    } else if (form.value.doc_tipo == 96) {
+      if (this.dniValido(form.value.doc_nro)) {
+        this.agregarCliente(form);
+      }
+    }
+    this.resetForm(form);
+  }
+
+  agregarCliente(form: NgForm) {
+    if (form.value.id == null) {
       this.clienteService.postCliente(form.value)
-      .subscribe(res => {
-        this.resetForm(form);
-        if (res == 1062) {
-          alert('No pudo crearse el producto. Ya existía un producto con el código ingresado.')
-        } else {
-          this.clientesObs.subscribe(cl => this.clientes = cl);
-        }
-      })
+        .subscribe(res => {
+          this.resetForm(form);
+          if (res == 1062) {
+            alert('No pudo crearse el producto. Ya existía un producto con el código ingresado.')
+          } else {
+            this.clientesObs.subscribe(cl => this.clientes = cl);
+          }
+        })
     }
     else {
       this.clienteService.putCliente(form.value)
-      .subscribe(res => {
-        this.resetForm(form);
-        if (res == 1062) {
-          alert('No pudo actualizarse el producto. Ya existía un producto con el código ingresado.')
-        } else {
-          this.clientesObs.subscribe(cl => this.clientes = cl);
-        }
-      })
+        .subscribe(res => {
+          this.resetForm(form);
+          if (res == 1062) {
+            alert('No pudo actualizarse el producto. Ya existía un producto con el código ingresado.')
+          } else {
+            this.clientesObs.subscribe(cl => this.clientes = cl);
+          }
+        })
     }
   }
 
-  editCliente(cliente: Cliente){
+  editCliente(cliente: Cliente) {
     this.clienteService.selectedCliente = new Cliente(cliente.id, cliente.nombre, cliente.doc_tipo, cliente.doc_nro, cliente.telefono, cliente.doc_descripcion);
   }
 
-  deleteCliente(id : number){
-    if (confirm('Desea eliminar el cliente?')){
+  deleteCliente(id: number) {
+    if (confirm('Desea eliminar el cliente?')) {
       this.clienteService.deleteCliente(id)
-      .subscribe(res => {
-        this.clientesObs.subscribe(cl => this.clientes = cl);
-      })
+        .subscribe(res => {
+          this.clientesObs.subscribe(cl => this.clientes = cl);
+        })
     }
   }
 
